1aef6127492b43cfcd17c593340f315df85411d1
michael <michael@substance.io>
2013-08-01 16:20:07 +0200

    Move document tests to article. Cyclic dependency fears.

---
+++ tests/index.js
@@ -0,0 +1,19 @@
+// Document Tests
+// ---------------
+
+require("./document");
+
+// Selection Tests
+// ---------------
+
+// require("./selection");
+
+// Writer Tests
+// ---------------
+
+
+// Actual tests
+// require("./writer/content_deletion");
+
+// require("./writer/content_deletion");
+// require("./writer/annotation_business");
