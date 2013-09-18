a5863f8dcc216f53a3d6d3477fa2a9e408a0760a
Michael Aufreiter <michael@substance.io>
2013-09-18 03:31:30 +0200

    Implement afterRender hook for Article.Renderer.

--- src/renderer.js
+++ src/renderer.js
@@ -5,9 +5,10 @@ var _ = require("underscore");
 // --------
 //
 
-var Renderer = function(docController) {
+var Renderer = function(docController, options) {
   this.docController = docController;
   // var that = this;
+  this.options = options || {};
 
   // TODO: use reflection
   this.nodeTypes = Article.nodeTypes;
@@ -59,9 +60,13 @@ Renderer.Prototype = function() {
 
     var docNodes = this.docController.container.getTopLevelNodes();
     _.each(docNodes, function(n) {
-      frag.appendChild(this.createView(n).render().el);
+      var view = this.createView(n);
+      frag.appendChild(view.render().el);
+      // Lets you customize the resulting DOM sticking on the el element
+      // Example: Lens focus controls
+      if (this.options.afterRender) this.options.afterRender(this.docController, view);
     }, this);
-
+    
     return frag;
   };
 
